87abaaf4cbf2b863ce268e278aba57c300c656c8
EliseoSantos2468 <sd20007@ues.edu.sv>
2025-11-20 19:55:17 -0600

    se agrego el servidor AAA

--- coreA/entrypoint.sh
+++ coreA/entrypoint.sh
@@ -12,17 +12,17 @@ ip link set eth2 up
 # Habilitar enrutamiento
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-# Limpiar default que ponga Docker
+# Limpiar default
 ip route del default 2>/dev/null || true
 
-# Rutas directas a sus redes locales
+# redes locales
 ip route add 10.0.10.0/24 dev eth0
 ip route add 10.0.30.0/24 dev eth2
 
-# 🔴 Ruta hacia LAN B (10.0.20.0/24) via coreB en la red de core
+# Ruta LAN B (10.0.20.0/24) via coreB en la red de core
 ip route add 10.0.20.0/24 via 10.0.40.3
 
-# (opcional) Default hacia el firewall
+# hacia el firewall
 ip route add default via 10.0.40.254
 
 echo "coreA READY"
--- coreB/entrypoint.sh
+++ coreB/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# IPs en cada interfaz
+# IPs
 ip addr add 10.0.20.254/24 dev eth0   # LAN B
 ip addr add 10.0.40.3/24  dev eth1   # Core / FW
 ip addr add 10.0.30.3/24  dev eth2   # SAN
@@ -13,14 +13,14 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 
 ip route del default 2>/dev/null || true
 
-# Rutas directas a sus redes locales
+# redes locales
 ip route add 10.0.20.0/24 dev eth0
 ip route add 10.0.30.0/24 dev eth2
 
-# 🔴 Ruta hacia LAN A (10.0.10.0/24) via coreA
+
 ip route add 10.0.10.0/24 via 10.0.40.2
 
-# (opcional) Default hacia el firewall
+# hacia el firewall
 ip route add default via 10.0.40.254
 
 echo "coreB READY"
